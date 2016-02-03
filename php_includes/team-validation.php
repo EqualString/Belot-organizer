dcf0d76c7b98edfbbdd2b3d7317b8e7eaa595e14
EqualString <alen.egredzija@gmail.com>
2016-02-03 16:51:10 +0100

    validation-mysqli

--- php_includes/team-validation.php
+++ php_includes/team-validation.php
@@ -4,8 +4,8 @@
 	if(isset($_POST["teamnamecheck"])){
 		$teamname = $_POST["teamnamecheck"];
 		$sql="SELECT * FROM `Teams` WHERE teamName='$teamname' LIMIT 1";
-		$result = mysql_query($sql) or die(mysql_error());
-		$count = mysql_num_rows($result);
+		$result = mysqli_query($sql);
+		$count = mysqli_num_rows($result);
 		if ($count == 1){ 
 			echo "1";
 			exit();
@@ -21,12 +21,12 @@
 		$teamusername = $_POST["tmu"];
 		
 		$sql="SELECT * FROM `Teams` WHERE email='$teammail' LIMIT 1";
-		$result = mysql_query($sql) or die(mysql_error());
-		$count = mysql_num_rows($result);
+		$result = mysqli_query($sql);
+		$count = mysqli_num_rows($result);
 		
 		$sql="SELECT * FROM `Teams` WHERE username='$teamusername' LIMIT 1";
-		$result = mysql_query($sql) or die(mysql_error());
-		$count2 = mysql_num_rows($result);
+		$result = mysqli_query($sql);
+		$count2 = mysqli_num_rows($result);
 		
 		if ($count == 1){ 
 			echo "1";
