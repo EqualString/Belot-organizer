b4259ff3b9bd4390c76745284441e63bf1eb868a
EqualString <alen.egredzija@gmail.com>
2016-02-04 02:50:59 +0100

    sdaf

--- router.php
+++ router.php
@@ -11,16 +11,6 @@
 		header("location: participant.php");
 	}
 	
-	//Router
-	require "php_includes/AltoRouter.php";
-	$router = new AltoRouter();
-	
-	$router->map( 'GET', '/', function() {
-		require __DIR__ . '/index.php';
-	});
-	$router->map( 'GET', '/home', function() {
-		require __DIR__ . 'router.php';
-	});
 ?>
 <!DOCTYPE HTML>
 <html>
