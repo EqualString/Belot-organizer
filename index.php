82f534fd3d04320916cfe96dfd0f3350e6ee9648
EqualString <alen.egredzija@gmail.com>
2016-02-04 02:30:46 +0100

    smz

--- index.php
+++ index.php
@@ -11,6 +11,12 @@
 		header("location: participant.php");
 	}
 	
+	include ("php_includes/AltoRouter.php");
+	$router = new AltoRouter();
+	$router->setBasePath('');
+
+	$router->map('GET','/', 'index.php', 'home');
+	$router->map('GET','/home/', 'index.php', 'home-home');
 ?>
 <!DOCTYPE HTML>
 <html>
